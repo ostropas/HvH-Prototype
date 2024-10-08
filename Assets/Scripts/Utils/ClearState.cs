942a51613e45a81b9cc010a90073ae4d5d3b92b9
Pavel Ostroukhov <p.ostroukhov@pp-team.com>
2024-10-08 15:07:12 +0300

    Fixes

---
+++ Assets/Scripts/Utils/ClearState.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Scripts.Utils {
+	public static class ClearState {
+		[MenuItem("Tools/Clear Save")]
+		public static void Clear() {
+			PlayerPrefs.DeleteAll();
+			PlayerPrefs.Save();
+
+			Caching.ClearCache();
+		}
+	}
+}
